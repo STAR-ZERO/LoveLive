7a840d9599d6d69a72267464f46c8c75425ff3ed
STAR_ZERO <kenji.01.star@gmail.com>
2014-03-02 13:17:44 +0900

    デザインを少し変更。ファイル構成変更。

--- config.rb
+++ config.rb
@@ -72,7 +72,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  activate :asset_hash, :ignore => %r{^images/member/.*}
 
   # Use relative URLs
   activate :relative_assets
---
+++ source/javascripts/item.js
@@ -0,0 +1,4 @@
+$(function() {
+  var member = Math.floor(Math.random() * 9) + 1;
+  $('#character img').attr('src', 'images/member/member' + member + '.png');
+});
